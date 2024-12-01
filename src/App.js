6bc669c528cba9da1de899d9ca9a61fabef57a76
Joshua <mutenjejoshua@gmail.com>
2024-12-01 12:49:11 +0200

    Hearder Check

--- src/App.js
+++ src/App.js
@@ -5,33 +5,25 @@ import Footer from './components/Footer';
 import Form from './components/Form';
 import DashBoard from './components/DashBoard';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-import VehicleManagementHome from './components/VehicleManagementHome';
-import VMH from './components/VMH';
-import TaskAssignmentPage from './components/TaskAssignmentPage';
-=======
 import FleetTracking from './components/FleetTracking';
->>>>>>> b3d746428e889e4c7539b8c2046cb987ed4d3444
+import AddVehicleForm from './components/AddVehicleForm';
 
 
 // First Login Page Components
-//      <Header/>
-//      <Form/>
-//<DashBoard/>
-//<AddVehicleForm/>
-//      <VMH/>
+
 //You can uncomment them, render them insisde the div instead of the dashboard component to see the Login Page
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <TaskAssignmentPage/>
-=======
       {/* <Header/> */}
-      <FleetTracking/>
+      <Header/>
+     <Form/>
+<DashBoard/>
+<AddVehicleForm/>
+     {/* <VMH/> */}
+      {/* <FleetTracking/> */}
       {/* <DashBoard/> */}
->>>>>>> b3d746428e889e4c7539b8c2046cb987ed4d3444
     </div>
   );
 }
