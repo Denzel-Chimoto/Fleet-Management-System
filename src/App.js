530a4c7f4548be510484c7f8fef4a39332ad56cf
Joshua <mutenjejoshua@gmail.com>
2024-12-01 12:52:56 +0200

    Merge with Routes

--- src/App.js
+++ src/App.js
@@ -3,36 +3,23 @@ import './App.css';
 import CreateUser from './components/CreateUser';
 import Form from './components/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-import FleetTracking from './components/FleetTracking';
-import AddVehicleForm from './components/AddVehicleForm';
-=======
 import { Routes, Route } from "react-router-dom";
 import VehicleManagementHome from './components/VehicleManagementHome';
 import AddVehicleForm from './components/AddVehicleForm';
 import DashBoard from './components/DashBoard';
 
->>>>>>> refs/remotes/origin/main
 
 
 // First Login Page Components
-
+//      <Header/>
+//      <Form/>
+//<DashBoard/>
+//<AddVehicleForm/>
+//      <VMH/>
 //You can uncomment them, render them insisde the div instead of the dashboard component to see the Login Page
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div>
-      {/* <Header/> */}
-      <Header/>
-     <Form/>
-<DashBoard/>
-<AddVehicleForm/>
-     {/* <VMH/> */}
-      {/* <FleetTracking/> */}
-      {/* <DashBoard/> */}
-    </div>
-=======
     <Routes>
     <Route path="/create-user" element={<CreateUser />} />
     <Route path="/login" element={<Form />} />
@@ -41,7 +28,6 @@ function App() {
     <Route path='/addVehicle' element={<AddVehicleForm/>} />
     <Route path='/dashboard' element={<DashBoard/>}/>
   </Routes>
->>>>>>> refs/remotes/origin/main
   );
 }
 
