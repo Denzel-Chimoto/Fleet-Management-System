e7c98d15e7ea73bde09da502b66a27061a661688
Joseph Chigova <josechigova@gmail.com>
2024-12-01 09:45:49 -0500

    removed some unnecessary icons, corrected overlapping

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,28 +3,20 @@ import Icon from './Icon';
 
 const Header = () => {
 
+  
   return (
 
-    <header>
+    <section>
       <div >
         <h1>Fleet Management System</h1>
       </div>
 
       <div>
-      <h1>Hi</h1>
         <Icon imgSrc={'vfmsIMG.jpg'}/>
-        <h1>Hi</h1>
       </div>
 
-      <div>
-        <Icon/>
-      </div>
-
-      <div>
-        <Icon/>
-      </div>
-
-    </header>
+      
+    </section>
   );
 }
 
