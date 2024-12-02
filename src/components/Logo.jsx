0f4e758e2f5fd99971ed9665e25af4dc569f0b71
Joshua <mutenjejoshua@gmail.com>
2024-12-02 11:33:45 +0200

    Styled Login Form, inserted Pictures designed in Photoshop as png,

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <div>
-        <img src="vfmsIMG.jpg" alt="A pic" />
+        <img src="vfmsIMG.png" alt="A pic" />
     </div>
   )
 }
